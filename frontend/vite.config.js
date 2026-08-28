3102fef8ada9c929e9c3beeefa7458ee21c2536e
naubass <naufalnajmi04@gmail.com>
2026-08-28 16:32:44 +0700

    fix static asset routing and spa fallback

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/', // Pastikan base adalah '/'
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
